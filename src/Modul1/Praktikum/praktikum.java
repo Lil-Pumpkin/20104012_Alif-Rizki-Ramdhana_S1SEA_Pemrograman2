b956c59d7fdb64375ab753f859ee2f19da264658
renzien <alifrizki1011@gmail.com>
2021-10-15 09:57:22 +0700

    add Modul1

---
+++ src/Modul1/Praktikum/praktikum.java
@@ -0,0 +1,7 @@
+package Modul1.Praktikum;
+
+public class praktikum {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Modul1/Tugas/Tugas.java
@@ -0,0 +1,7 @@
+package Modul1.Tugas;
+
+public class Tugas {
+    public static void main(String[] args) {
+        
+    }
+}
